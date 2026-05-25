d9ab1305e61b4600c7b2d12b648fa86d7ae63a0e
Chirag Patel <chiragpatel499@gmail.com>
2026-05-25 10:17:37 +0530

    domain update

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,3 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views.
-"""
-
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
@@ -12,16 +6,12 @@ from django.conf.urls.static import static
 from api.views import frontend
 
 urlpatterns = [
-    # Django Admin
     path('admin/', admin.site.urls),
 
-    # API Routes
     path('api/', include('api.urls')),
 
-    # React Frontend Catch-All
-    re_path(r'^.*$', frontend),
+    # React frontend catch-all
+    re_path(r'^(?!admin/?$|admin/|api/).*$' , frontend),
 ]
 
-# Media Files
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
